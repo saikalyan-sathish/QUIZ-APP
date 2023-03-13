3e7e68cb1e681d6966c01b185bf1d3a38cf0f22e
SAIKALYAN SATHISH <saikalyansathish@SAIKALYANs-MacBook-Pro.local>
2023-03-13 22:42:26 +0530

    edited the home.css and firebase

--- src/components/Admin/AdminDash.js
+++ src/components/Admin/AdminDash.js
@@ -1,37 +1,86 @@
-import React from "react";
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import '@fontsource/roboto/400.css';
-import "./Admin.css";
+import React, { useState } from "react";
+import { initializeApp, getApps } from "firebase/app";
+import { getDatabase, ref, set ,push, child} from "firebase/database";
 
-function AdminDash() {
+function AdminPage() {
+  const [question, setQuestion] = useState("");
+  const [options, setOptions] = useState([]);
+  const [correctAnswers, setCorrectAnswers] = useState([]);
+  const [key, setKey] = useState("");
+
+  const handleQuestionChange = (e) => {
+    setQuestion(e.target.value);
+  };
+
+  const handleOptionChange = (e) => {
+    let updatedOptions = [...options];
+    updatedOptions[e.target.id] = e.target.value;
+    setOptions(updatedOptions);
+  };
+
+  const handleCorrectAnswerChange = (e) => {
+    let updatedCorrectAnswers = [...correctAnswers];
+    if (e.target.checked) {
+      updatedCorrectAnswers.push(e.target.value);
+    } else {
+      updatedCorrectAnswers = updatedCorrectAnswers.filter(
+        (item) => item !== e.target.value
+      );
+    }
+    setCorrectAnswers(updatedCorrectAnswers);
+  };
+const db = getDatabase();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const db = getDatabase();
+    let newKey = db.ref().child("questions").push().key;
+    // let newKey = set(ref(db),child("questions"),push().key);
+    setKey(newKey);
+   set(ref(db,"questions/"+newKey),{
+      question: question,
+      options: options,
+      correctAnswers: correctAnswers,
+    });
+    setQuestion("");
+    setOptions([]);
+    setCorrectAnswers([]);
+  };
+  // db.ref("questions/" + newKey).
+
   return (
     <div>
-      <div className="Heading">
-      <h2>Please enter the questions to be displayed in the Quiz</h2></div>
-   <div className="form">
-        
-          <TextField
-            id="name-input"
-            name="name"
-            label="Enter the question"
-            type="text"
-          
-          />
-        
-        <li></li>
-        
-          <TextField
-            id="age-input"
-            name="age"
-            label="Age"
-            type="number"/>
-           
-           </div>
-          
+      <form onSubmit={handleSubmit}>
+        <label>Question:</label>
+        <input type="text" value={question} onChange={handleQuestionChange} />
+        <br />
+        <br />
+        <label>Options:</label>
+        {options.map((option, index) => (
+          <div key={index}>
+            <input
+              type="text"
+              id={index}
+              value={option}
+              onChange={handleOptionChange}
+            />
+            <input
+              type="checkbox"
+              value={option}
+              onChange={handleCorrectAnswerChange}
+            />
+            Correct
+          </div>
+        ))}
+        <br />
+        <button type="button" onClick={() => setOptions([...options, ""])}>
+          Add Option
+        </button>
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
 
-export default AdminDash;
+export default AdminPage;
--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCWVcist9eDsISGSlgSZ3FCuJMAfV42YAg",
   authDomain: "quiz-demo-96fc0.firebaseapp.com",
+  databaseURL: "https://quiz-demo-96fc0-default-rtdb.firebaseio.com",
   projectId: "quiz-demo-96fc0",
   storageBucket: "quiz-demo-96fc0.appspot.com",
   messagingSenderId: "128317753008",
@@ -13,4 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+const database = getDatabase(app);
 export default app;
